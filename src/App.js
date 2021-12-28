4658c69babee6c6830f6a7b7f1da615479c5d418
LE TAN LOC <letanlocjp1001@gmail.com>
2021-12-28 23:20:49 +0900

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,52 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
-  return <h2>quiz starter</h2>
+  const {
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    nextQuestion,
+    checkAnswer,
+  } = useGlobalContext()
+  // console.log(questions)
+  if (waiting) {
+    return <SetupForm />
+  }
+  if (loading) {
+    return <Loading />
+  }
+  const { question, incorrect_answers, correct_answer } = questions[index]
+  const answers = [...incorrect_answers, correct_answer]
+  return (
+    <main>
+      {/* <Modal /> */}
+      <section className='quiz'>
+        <p className='correct-answers'>
+          correct answers: {correct}/{index}
+        </p>
+        <article className='container'>
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className='btn-container'>
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className='answer-btn'
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                  onClick={() => checkAnswer(correct_answer)}
+                />
+              )
+            })}
+          </div>
+        </article>
+        <button className='next-question' onClick={nextQuestion}>
+          next question
+        </button>
+      </section>
+    </main>
+  )
 }
 
 export default App
--- src/context.js
+++ src/context.js
@@ -15,7 +15,73 @@ const tempUrl =
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [waiting, setWaiting] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [index, setIndex] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const [error, setError] = useState(false)
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const fetchQuestions = async (url) => {
+    setLoading(true)
+    setWaiting(false)
+    const response = await axios(url).catch((err) => console.log(err))
+    if (response) {
+      const data = response.data.results
+      if (data.length > 0) {
+        setQuestions(data)
+        setLoading(false)
+        setWaiting(false)
+        setError(false)
+      } else {
+        setWaiting(true)
+        setError(false)
+      }
+    } else {
+      setWaiting(true)
+    }
+  }
+
+  const nextQuestion = () => {
+    setIndex((oldIndex) => {
+      const index = oldIndex + 1
+      if (index > questions.length - 1) {
+        //openModal()
+        return 0
+      } else {
+        return index
+      }
+    })
+  }
+
+  const checkAnswer = (value) => {
+    if (value) {
+      setCorrect((oldState) => oldState + 1)
+    }
+    nextQuestion()
+  }
+
+  useEffect(() => {
+    fetchQuestions(tempUrl)
+  }, [])
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+        nextQuestion,
+        checkAnswer,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
 }
 // make sure use
 export const useGlobalContext = () => {
